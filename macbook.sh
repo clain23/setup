adac47f2dbd8db0425f6d079f488d5923b8f3145
Igor Kudrin <clain@clain23.ru>
2019-08-30 00:04:26 +0300

    Create macbook.sh

---
+++ macbook.sh
@@ -0,0 +1,13 @@
+# Install Homebrew
+
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Install software
+
+brew update
+brew cask install 1password
+brew cask install adobe-acrobat-reader
+brew cask install caffeine
+brew cask install docker
+brew cask install google-chrome
+brew cask install powershell
