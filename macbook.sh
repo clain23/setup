c83efadaaa8d85e72a8911d7de4d13a57608733e
Igor Kudrin <clain@clain23.ru>
2019-08-30 00:22:50 +0300

    Update macbook.sh

--- macbook.sh
+++ macbook.sh
@@ -11,7 +11,7 @@ brew cask install caffeine
 brew cask install docker
 brew cask install google-chrome
 brew cask install powershell
-brew cask install telegram
+#brew cask install telegram #rkn problems
 brew cask install visual-studio-code
 
 # Setup autoupdate
