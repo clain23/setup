202c7afb6d2f797db7f4224f65291d67f84f87d2
Igor Kudrin <clain@clain23.ru>
2020-03-02 12:25:52 +0300

    Update macbook.sh

--- macbook.sh
+++ macbook.sh
@@ -71,6 +71,10 @@ code --install-extension mauve.terraform
 
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# Add the plugin to the list of plugins for Oh My Zsh to load (inside ~/.zshrc):
+# plugins=(zsh-autosuggestions)
 
 #minikube setup
 
